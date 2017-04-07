3e2205a9598c5416cbcb1559a7a992f5a4dda14b
Yann Guineau <yann.guineau@ynov.com>
2017-04-07 21:01:58 +0000

    restore routes

--- server.js
+++ server.js
@@ -32,6 +32,10 @@ const userCtrl    = require("./controller/userController");
 const chatCtrl    = require("./controller/chatController");
 const projectCtrl = require("./controller/projectController");
 
+router.use('/user', userCtrl);
+router.use('/chat', chatCtrl);
+router.use('/project', projectCtrl);
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("-----------------------------------");
   console.log("Serveur en écoute sur le port " + process.env.PORT);
