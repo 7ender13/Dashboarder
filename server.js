e55bc25ca343b8a2566a3b9473f77bc515b7484f
Yann Guineau <yann.guineau@ynov.com>
2017-04-07 20:53:11 +0000

    update server

--- server.js
+++ server.js
@@ -3,17 +3,19 @@
 const http          = require('http');
 const path          = require('path');
 const mongoose      = require('mongoose');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const socketio      = require('socket.io');
 
-let express         = require('express');
-let bodyParser      = require('body-parser');
-const router        = express();
+const router = express();
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-let server          = http.createServer(router);
 
-const userCtrl      = require("./controller/userController");
+let server = http.createServer(router);
 
-//MongoDB : Collection "DashBoard" créé pour le projet
+const userCtrl = require("./controller/userController");
+
 mongoose.connect('mongodb://localhost/DashBoard', (error)=> {
     if(error){
         console.log("Connection database : NOK !");
@@ -27,9 +29,41 @@ mongoose.connect('mongodb://localhost/DashBoard', (error)=> {
 router.use(express.static(path.join(__dirname, 'client/dist')));
 router.use('/user', userCtrl);
 
-
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log("-----------------------------------");
   console.log("Serveur en écoute sur le port " + process.env.PORT);
   console.log("-----------------------------------");
 });
+
+let io = socketio.listen(server);
+
+io.on('connection', function(socket){
+    console.log("client : "+socket.request.connection.remoteAddress+' connected');
+    socket.join(0);
+    /*MESSAGE PART*/
+    socket.on('goToRoom', function(data){
+        socket.join(data.projectID);
+        socket.room = data.projectID;
+        console.log('go to room : '+data.projectID)
+
+        //init client message
+        var conversation = {
+            'participant' : [{'username' : 'Niko', 'status' : true}, {'username' : 'Yann Guineau', 'status' : false}],
+            'messages' : [
+                {'from' : 'Thomas Burgio', 'message' : 'nouveau message'},
+                {'from' : 'Yann', 'message' : 'hello'},
+                {'from' : 'Cyril Potdevin', 'message' : 'Salut'},
+                {'from' : 'Thomas Burgio', 'message' : 'hello'},
+                {'from' : 'Thomas Burgio', 'message' : 'hello'}
+            ]
+        };
+        socket.emit('initMessage', conversation);
+    })
+
+    socket.on('newMessage', function(data){
+        //save to BDD
+        console.log(data);
+        var messageObj = {'from' : data.from, 'message' : data.message}
+        io.sockets.in(socket.room).emit('messageReceive',messageObj);
+    });
+})
