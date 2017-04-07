01d44267cb1c33cda811dd3d1022a53f5d3c0461
Nicolas <nicolas.dufour@ynov.com>
2017-04-07 16:20:14 +0000

    ProjectController & server change

--- controller/projectController.js
+++ controller/projectController.js
@@ -4,10 +4,82 @@ const express   = require('express')
 const router    = express.Router();
 
 const projectModel  = require('../model/projectModel');
+const userModel     = require('../model/userModel');
 
 const mongoose      = require('mongoose');
 
-
+router.post("/", (req, res) => {
+    console.log("-->-->-->-->-->-->-->-->-->--");
+    console.log("project/post");
+    console.log("-->-->-->-->-->-->-->-->-->--");
+    
+    console.log(req.body);
+    
+    userModel.findById(req.body.creatorID, (err, result)=>{
+        if(err){
+            console.log(err);
+            console.log("erreur");
+            res.status(409);
+            res.end();
+            
+        }
+        else
+        {
+            console.log("success");
+            console.log(result);
+        
+            var pseudos = '{';
+            for(var i = 0; i < req.body.participants.length; i++)
+            {
+                if(i != 0)
+                {
+                    pseudos += ", ";
+                }
+                userModel.findById(req.body.participants[i].id, (err, result) => {
+                    if(err){
+                        console.log("erreur");
+                        console.log(err);
+                    }
+                    pseudos += "pseudo";
+                    console.log("azeaze");
+                    
+                });
+            }
+            
+            pseudos += "}";
+            
+            console.log("test");
+            console.log(pseudos);
+            
+            var project = new projectModel({
+                name:req.name,
+                creatorName:result.name,
+                description:req.description,
+                
+                });
+    
+            // project.save(function (err) {
+            //     if (err) 
+            //     { 
+            //         res.status(500);
+            //         console.log("erreur");
+            //         console.log(err);
+            //         res.end();
+            //     }
+            //     else
+            //     {
+            //         res.status(200);
+            //         console.log('Projet Ajouté dans mongoDB !');
+            //         console.log(project);
+            //         res.end();
+            //     }
+            // });
+            
+            res.status(200);
+            res.end();
+        }
+    });
+});
 
 
 
--- server.js
+++ server.js
@@ -12,20 +12,26 @@ router.use(bodyParser.urlencoded({ extended: true }));
 let server          = http.createServer(router);
 
 const userCtrl      = require("./controller/userController");
+const chatCtrl      = require("./controller/chatController");
+const projectCtrl      = require("./controller/projectController");
 
 //MongoDB : Collection "DashBoard" créé pour le projet
 mongoose.connect('mongodb://localhost/DashBoard', (error)=> {
-    if(error){
+    if(error)
+    {
         console.log("Connection database : NOK !");
         console.log(error)
     }
-    else{
+    else
+    {
         console.log("Connection database : OK !");
     }
 });
 
-router.use(express.static(path.join(__dirname, 'client')));
+router.use(express.static(path.join(__dirname, 'client/dist')));
 router.use('/user', userCtrl);
+router.use('/chat', chatCtrl);
+router.use('/project', projectCtrl);
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
