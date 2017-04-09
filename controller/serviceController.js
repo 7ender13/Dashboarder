6b15386dc90bc759ec4a89f6dbb258da126799f1
Thibaut Taelman <thibaut.taelman@ynov.com>
2017-04-09 20:18:13 +0000

    rectif

--- controller/serviceController.js
+++ controller/serviceController.js
@@ -17,16 +17,22 @@ client.on('projectaUpdated', (data) => {
     updateProject(data);
 });
 
+//Controller **SERVICE**
+
+// Méthode `GET` permet de créer un service  avec le nom
 router.get("/:name", (req, res) => {
     console.log("-->-->-->-->-->-->-->-->-->--");
     console.log("service/get (all except name)");
     console.log("-->-->-->-->-->-->-->-->-->--");
     
+    // On récupère le 1er paramètre de l'`URL`, le _nom_ du service
     let nameOfMyService = req.url.split("/")[1];
     
+    // On cherche le _projet_ en question,s'il n'existe pas déjà
     serviceModel.find({nameService:{$not:nameOfMyService}}, (err, result) => {
         if(err)
         {
+             //si on en trouve un, on retourne erreur `HTTP` 409 : CONFLICT
             console.log("erreur dans le get");
             console.log(err);
             res.status(409);
@@ -34,6 +40,7 @@ router.get("/:name", (req, res) => {
         }
         else
         {
+            // S'il n'y a pas d'erreur, on retourne un `HTTP` 200 : OK avec le resultat
             res.status(200);
             res.json(result);
             res.end();
@@ -42,17 +49,19 @@ router.get("/:name", (req, res) => {
     });
 });
 
-
+// **fonction** updateProject, permet de mettre à jour le projet concerné
 function updateProject(data)
 {
     for(var i = 0; i < data.length; i++)
     {
+        // on créé un service avec son modèle et les data en paramètre
         console.log(data[i]);
         let service = new serviceModel({
             nameService:data[i].nameOfService,
             projects:data[i].projects
         });
         
+        // on sauvegarde le service créé
         service.save((err) => {
             if(err)
             {
